b7597e291fe5626845a7540fe72c64e1808dd0b3
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 15:02:44 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+if block_given?
+  i = 0 
+  array = []
+  
+  while i < collection.length do
+    if yield(collection[i])
+      array.push(collection[i])
+    end
+    i += 1 
+  end
+ array
+ else 
+   "This block should not run"
+  
 end
+end
+
+my_select(["hello", "hallo", "yellaf"])
